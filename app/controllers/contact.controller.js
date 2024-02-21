63c1768c14b92781ca94ea35c06aad3abaa3a969
nhathaocute <nhathao0945477411@gmail.com>
2024-02-21 10:12:51 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create = (req, res) => {
+  res.sen({ message: "create handler" });
+};
+exports.findAll = (req, res) => {
+  res.sen({ message: "findAll handler" });
+};
+exports.findOne = (req, res) => {
+  res.sen({ message: "findOne handler" });
+};
+exports.update = (req, res) => {
+  res.sen({ message: "update handler" });
+};
+exports.delete = (req, res) => {
+  res.sen({ message: "delete handler" });
+};
+exports.deleteAll = (req, res) => {
+  res.sen({ message: "deleteAll handler" });
+};
+exports.findAllFavorite = (req, res) => {
+  res.sen({ message: "findAllFavorite handler" });
+};
